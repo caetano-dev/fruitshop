7304b141f8aa22ac5d9b84d11113a3c67e3c78a9
drull <pcaetanop@protonmail.com>
2023-11-03 16:05:24 -0300

    Initial Commit

---
+++ foodshop/foodshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodshopApp.swift
+//  foodshop
+//
+//  Created by Pedro Caetano on 03/11/23.
+//
+
+import SwiftUI
+
+@main
+struct foodshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
